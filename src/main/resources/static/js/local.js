0a6902910f757bbc2223ffd2feb369a793379b5f
SCIS3-PC02\SCIS3-PC02 <dandre91y_x792c@tigpe.com>
2022-04-21 19:42:26 -0500

    parcial

--- src/main/resources/static/js/local.js
+++ src/main/resources/static/js/local.js
@@ -15,3 +15,20 @@ async function sendData(path){
     });
     console.log(await request.text())
 }
+async function sendData(path){
+    var myForm = document.getElementById("myForm");
+    var formData= new FormData(myForm);
+    var jsonData={};
+    for(var [k, v] of formData){
+        jsonData[k]=v;
+    }
+    const request= await fetch (path,{
+        method:'POST',
+        headers:{
+            'Accept':'application/json',
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify(jsonData)
+    });
+    console.log(await request.text())
+}
